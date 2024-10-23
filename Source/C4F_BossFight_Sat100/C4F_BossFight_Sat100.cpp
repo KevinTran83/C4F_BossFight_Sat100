736cffb82bd7e12c9c606994ffd8d0635871ff0e
Kevin S. Tran <tanksense@gmail.com>
2024-10-23 18:43:34 +1100

    Init

---
+++ Source/C4F_BossFight_Sat100/C4F_BossFight_Sat100.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "C4F_BossFight_Sat100.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, C4F_BossFight_Sat100, "C4F_BossFight_Sat100" );
